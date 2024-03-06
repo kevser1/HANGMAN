24bafac2cbc1b2a3373c4c4d36c2dc516798b07d
kevser1 <100615219+kevser1@users.noreply.github.com>
2024-03-06 12:25:45 +0300

    Add files via upload
    hangman

---
+++ hangman.py
@@ -0,0 +1,58 @@
+import random
+
+
+from hangman_words import word_list
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+
+end_of_game = False
+lives = 6
+
+
+from hangman_art import logo
+print(logo)
+
+
+display = []
+for _ in range(word_length):
+    display += "_"
+#listeye range fonksiyonu kullanarak zaten kelimenin uzunluğu için oluşturduğumuz word_lenght ile birlikte karakter sayısı kadar "_" ekliyoruz
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+#kullanıcının girdiği harfle o an ki pozisyondaki kelime harfinin karşılaştırılmasına olanak tanır.
+#bu sayede döngü her bir harfi tek tek kontrol eder bu kullanıcının girdiği harfle o anki pozisyonda olan harfi karşılaştırır.
+    #Check if user is wrong.
+    if guess not in chosen_word:
+   
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+   
+    print(f"{' '.join(display)}")
+
+   
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    
+    from hangman_art import stages
+    print(stages[lives])
